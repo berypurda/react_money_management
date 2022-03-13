fba72f083cfc465823ac3ed78a08e9e85aab4761
berypurda <berypurda@gmail.com>
2022-03-13 14:07:41 +0100

    signUp hook complete

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBENdmJlE0CC6fKBlrNLHD8Yf42_HkNUfw",
+  authDomain: "moneymanagement-bc671.firebaseapp.com",
+  projectId: "moneymanagement-bc671",
+  storageBucket: "moneymanagement-bc671.appspot.com",
+  messagingSenderId: "984646572430",
+  appId: "1:984646572430:web:776cd90e41760163566cdf"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init service
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -1,7 +1,45 @@
+import { useState } from 'react'
+
 import styles from './SignUp.module.css'
 
 export default function SignUp() {
+  const [email, setEmail] = useState('')
+  const [DisplayName, setDisplayName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, DisplayName, password)
+  }
+
   return (
-    <div>SignUp</div>
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>SignUp</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>DisplayName:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={DisplayName}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">SignUp</button>
+    </form>
   )
 }
